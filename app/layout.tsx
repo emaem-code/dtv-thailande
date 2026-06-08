ef4dd07e2f7fc2bf8e1a64ff0e777883c703c3d8
matthieu moretti <matthieumoretti@macbook-pro-de-matthieu.home>
2026-06-08 13:34:23 +0200

    fix css problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+// @ts-ignore: CSS module declarations are not present in this project setup.
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
